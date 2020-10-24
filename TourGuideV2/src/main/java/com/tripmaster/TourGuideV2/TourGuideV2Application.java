51305d65f5d2ad0008a0406e40d384e6868e425a
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-24 10:51:22 +0200

    feat: create TourGuideSuite2020 project base

---
+++ TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/TourGuideV2Application.java
@@ -0,0 +1,13 @@
+package com.tripmaster.TourGuideV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourGuideV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourGuideV2Application.class, args);
+	}
+
+}
---
+++ TourGuideV2/src/test/java/com/tripmaster/TourGuideV2/TourGuideV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripmaster.TourGuideV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourGuideV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

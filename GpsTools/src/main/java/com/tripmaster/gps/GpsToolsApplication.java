386cdefc386bebedce55cde421424db9a0afbd8f
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-24 11:38:40 +0200

    feat: add GpsTools project

---
+++ GpsTools/src/main/java/com/tripmaster/gps/GpsToolsApplication.java
@@ -0,0 +1,13 @@
+package com.tripmaster.gps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsToolsApplication.class, args);
+	}
+
+}

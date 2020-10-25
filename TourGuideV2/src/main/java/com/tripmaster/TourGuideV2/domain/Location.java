9dcbb8c1e4c1d4dc28512605efe7c651a8ce6198
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-25 18:27:34 +0100

    feat: add 3 new classes in TourGuideV2 domain corresponding to gpsUtil.jar classes

---
+++ TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/domain/Location.java
@@ -0,0 +1,85 @@
+package com.tripmaster.TourGuideV2.domain;
+
+import java.util.UUID;
+
+/**
+ * Domain class used to store the data of gpsUtil Location class.
+ *
+ * @author Thierry SCHREINER
+ * @version 1.0
+ * @since October 2020
+ */
+public class Location {
+
+    UUID userId;
+    /**
+     * The latitude of the Global Position location.
+     */
+    double latitude;
+
+    /**
+     * The longitude of the Global Position location.
+     */
+    double longitude;
+
+    /**
+     * Class constructor with 2 parameters.
+     *
+     * @param pLatitude
+     * @param pLongitude
+     */
+    public Location(final double pLatitude, final double pLongitude) {
+        userId = UUID.randomUUID();
+        latitude = pLatitude;
+        longitude = pLongitude;
+    }
+
+    /**
+     * Class constructor with 3 parameters.
+     *
+     * @param pUserId
+     * @param pLatitude
+     * @param pLongitude
+     */
+    public Location(final UUID pUserId, final double pLatitude,
+            final double pLongitude)
+    {
+        userId = pUserId;
+        latitude = pLatitude;
+        longitude = pLongitude;
+    }
+
+    /**
+     * No argument and protected empty constructor.
+     */
+    protected Location() {
+    }
+
+    /**
+     * Getter of latitude attribute.
+     *
+     * @return a double between -90 and +90
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Getter of longitude attribute.
+     *
+     * @return a double between -180 and +180
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Serialization method.
+     */
+    @Override
+    public String toString() {
+        return "Location [latitude=" + latitude + ", longitude=" + longitude
+                + "]";
+    }
+
+}
---
+++ TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/domain/VisitedLocation.java
@@ -0,0 +1,90 @@
+package com.tripmaster.TourGuideV2.domain;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * DTO used to transfer the data of a gpsUtil VisitedLocation class, witch is
+ * the location where a user was at such time.
+ *
+ * @author Thierry SCHREINER
+ * @version 1.0
+ * @since October 2020
+ */
+public class VisitedLocation {
+
+    /**
+     * 
+     */
+    UUID userId;
+    /**
+     * This attribute tells us when this visitedLocation has been collected by
+     * gpsUtil.
+     */
+    Date timeVisited;
+
+    /**
+     * The location(latitude and longitude) of the VisitedLocation.
+     */
+    Location location;
+
+    /**
+     * Class constructor with 2 parameters..
+     *
+     * @param pTimeVisited
+     * @param pLocation
+     */
+    public VisitedLocation(final Location pLocation, final Date pTimeVisited)
+    {
+        userId = UUID.randomUUID();
+        timeVisited = pTimeVisited;
+        location = pLocation;
+    }
+
+    /**
+     * No argument and protected empty class constructor.
+     */
+    protected VisitedLocation() {
+    }
+
+    /**
+     * Full parameters class constructor.
+     *
+     * @param pUserId
+     * @param pLocation
+     * @param pTimeVisited
+     */
+    public VisitedLocation(UUID pUserId, Location pLocation, Date pTimeVisited) {
+        userId = pUserId;
+        timeVisited = pTimeVisited;
+        location = pLocation;
+    }
+
+    /**
+     * Getter of timeVisited attribute.
+     *
+     * @return a LocalDateTime
+     */
+    public Date getTimeVisited() {
+        return timeVisited;
+    }
+
+    /**
+     * Getter of timeVisited attribute.
+     *
+     * @return a LocationDTO
+     */
+    public Location getLocation() {
+        return location;
+    }
+
+    /**
+     * Serialization method.
+     */
+    @Override
+    public String toString() {
+        return "VisitedLocation [location=" + location.toString()
+                + ", timeVisited=" + timeVisited + "]";
+    }
+
+}

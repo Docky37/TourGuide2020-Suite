de816aa02b879c0aa8d4af61f8e87c3a156d9d3a
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-25 19:22:11 +0100

    feat: add InternalTestHelper from old project

---
+++ TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/helper/InternalTestHelper.java
@@ -0,0 +1,35 @@
+package com.tripmaster.TourGuideV2.helper;
+
+/**
+ * This class only contains one private static attribute, the number of internal
+ * users to create for tests (default value = 100), and its getter & setter.
+ *
+ * @author TripMaster
+ * @author Thierry Schreiner
+ */
+public class InternalTestHelper {
+
+    /**
+     * The number of internal users to create for tests.
+     * (Set this default up to 100,000 for testing)
+     */
+    private static int internalUserNumber = 100;
+
+    /**
+     * Setter of internalUserNumber.
+     *
+     * @param internalUserNumber
+     */
+    public static void setInternalUserNumber(int internalUserNumber) {
+        InternalTestHelper.internalUserNumber = internalUserNumber;
+    }
+
+    /**
+     * Getter of internalUserNumber.
+     *
+     * @return an int
+     */
+    public static int getInternalUserNumber() {
+        return internalUserNumber;
+    }
+}

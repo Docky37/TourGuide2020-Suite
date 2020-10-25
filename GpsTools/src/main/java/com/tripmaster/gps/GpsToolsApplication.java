8e0a8e536d5e295961b3ac673dd80c6d60f85072
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-25 02:39:44 +0200

    feat: add Locale.setDefault(Locale.US) in GpsToolsApplication

--- GpsTools/src/main/java/com/tripmaster/gps/GpsToolsApplication.java
+++ GpsTools/src/main/java/com/tripmaster/gps/GpsToolsApplication.java
@@ -1,5 +1,7 @@
 package com.tripmaster.gps;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GpsToolsApplication {
 
 	public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 		SpringApplication.run(GpsToolsApplication.class, args);
 	}
 

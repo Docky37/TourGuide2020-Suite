14452c35e94088fe24a2210cd310bb9fb44a00bf
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-26 00:47:45 +0100

    feat: add 3 DTO to accept data from TourGuideV2

---
+++ GetRewards/src/main/java/com/tripmaster/rewards/dto/AttractionDTO.java
@@ -0,0 +1,119 @@
+package com.tripmaster.rewards.dto;
+
+/**
+ * DTO used to transfer the data of gpsUtil Attraction class.
+ *
+ * @author Thierry SCHREINER
+ * @version 1.0
+ * @since October 2020
+ */
+public class AttractionDTO {
+
+    /**
+     * The name of this attraction.
+     */
+    private String attractionName;
+
+    /**
+     * The city where this attraction is located in.
+     */
+    private String city;
+
+    /**
+     * The location's latitude of this attraction.
+     */
+    private double latitude;
+
+    /**
+     * The location's longitude of this attraction.
+     */
+    private double longitude;
+
+    /**
+     * The state where this attraction is located in.
+     */
+    private String state;
+
+    /**
+     * Full parameters class constructor.
+     *
+     * @param pAttractionId
+     * @param pAttractionName
+     * @param pCity
+     * @param pLatitude
+     * @param pLongitude
+     * @param pState
+     */
+    public AttractionDTO(final String pAttractionName,
+            final String pCity, final String pState,
+            final double pLatitude, final double pLongitude)
+    {
+        attractionName = pAttractionName;
+        city = pCity;
+        state = pState;
+        latitude = pLatitude;
+        longitude = pLongitude;
+    }
+
+    /**
+     * No argument and protected empty class constructor.
+     */
+    protected AttractionDTO() {
+    }
+
+    /**
+     * Getter of the attractionName attribute.
+     *
+     * @return a String
+     */
+    public String getAttractionName() {
+        return attractionName;
+    }
+
+    /**
+     * Getter of the city attribute.
+     *
+     * @return a String
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * Getter of the state attribute.
+     *
+     * @return a String
+     */
+    public String getState() {
+        return state;
+    }
+
+    /**
+     * Getter of the location's latitude.
+     *
+     * @return a double
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Getter of the location's longitude.
+     *
+     * @return a double
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * Serialization method.
+     */
+    @Override
+    public String toString() {
+        return "AttractionDTO [attractionName=" + attractionName + ", city="
+                + city + ", state=" + state + ", latitude=" + latitude
+                + ", longitude=" + longitude + "]";
+    }
+
+}
---
+++ GetRewards/src/main/java/com/tripmaster/rewards/dto/VisitedLocationDTO.java
@@ -0,0 +1,72 @@
+package com.tripmaster.rewards.dto;
+
+import java.util.Date;
+
+/**
+ * DTO used to transfer the data of a gpsUtil VisitedLocation class, witch is
+ * the location where a user was at such time.
+ *
+ * @author Thierry SCHREINER
+ * @version 1.0
+ * @since October 2020
+ */
+public class VisitedLocationDTO {
+
+    /**
+     * This attribute tells us when this visitedLocation has been collected by
+     * gpsUtil.
+     */
+    private Date timeVisited;
+
+    /**
+     * The location(latitude and longitude) of the VisitedLocation.
+     */
+    private LocationDTO location;
+
+    /**
+     * Full parameters class constructor.
+     *
+     * @param pTimeVisited
+     * @param pLocation
+     */
+    public VisitedLocationDTO(final LocationDTO pLocation,
+            final Date pTimeVisited)
+    {
+        timeVisited = pTimeVisited;
+        location = pLocation;
+    }
+
+    /**
+     * No argument and protected empty class constructor.
+     */
+    protected VisitedLocationDTO() {
+    }
+
+    /**
+     * Getter of timeVisited attribute.
+     *
+     * @return a LocalDateTime
+     */
+    public Date getTimeVisited() {
+        return timeVisited;
+    }
+
+    /**
+     * Getter of timeVisited attribute.
+     *
+     * @return a LocationDTO
+     */
+    public LocationDTO getLocation() {
+        return location;
+    }
+
+    /**
+     * Serialization method.
+     */
+    @Override
+    public String toString() {
+        return "VisitedLocationDTO [location=" + location.toString()
+                + ", timeVisited=" + timeVisited + "]";
+    }
+
+}

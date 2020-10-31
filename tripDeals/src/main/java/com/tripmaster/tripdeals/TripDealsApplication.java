1ae06b6d8a2dcb4aa83d29c0ea7e6afe6ea153fd
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-10-31 20:25:55 +0100

    feat: create new TripDeals application and add TripPricer jar

---
+++ tripDeals/src/main/java/com/tripmaster/tripdeals/TripDealsApplication.java
@@ -0,0 +1,13 @@
+package com.tripmaster.tripdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripDealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripDealsApplication.class, args);
+	}
+
+}
---
+++ tripDeals/src/test/java/com/tripmaster/tripdeals/TripDealsProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripmaster.tripdeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripDealsProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

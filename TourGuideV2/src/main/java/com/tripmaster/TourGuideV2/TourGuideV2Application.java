357719972080e90d4df80617cad618be0b2dd5eb
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-11-09 16:29:55 +0100

    refactor: resolve CheckStyle issues in TourGuideV2Application.java

--- TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/TourGuideV2Application.java
+++ TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/TourGuideV2Application.java
@@ -3,12 +3,29 @@ package com.tripmaster.TourGuideV2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class contains the main method used to run the application.
+ *
+ * @author TripMaster
+ * @author Thierry Schreiner
+ */
 @SpringBootApplication
-public class TourGuideV2Application {
+public final class TourGuideV2Application {
 
-	public static void main(String[] args) {
-	    System.setProperty("spring.devtools.restart.enabled", "false");
-	    SpringApplication.run(TourGuideV2Application.class, args);
-	}
+    /**
+     * Main method that provides the entry point of this Spring application.
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(TourGuideV2Application.class, args);
+    }
+
+    /**
+     * Empty class constructor.
+     */
+    private TourGuideV2Application() {
+    }
 
 }

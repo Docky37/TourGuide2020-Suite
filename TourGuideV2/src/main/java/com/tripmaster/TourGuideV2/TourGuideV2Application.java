11b16720d2a9516583445b9953ee1ca749afe824
Thierry Schreiner <Thierry.Schreiner@hotmail.fr>
2020-11-21 10:43:04 +0100

    doc: add and configure Swagger v3.0

--- TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/TourGuideV2Application.java
+++ TourGuideV2/src/main/java/com/tripmaster/TourGuideV2/TourGuideV2Application.java
@@ -1,7 +1,16 @@
 package com.tripmaster.TourGuideV2;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 /**
  * This class contains the main method used to run the application.
@@ -26,6 +35,30 @@ public class TourGuideV2Application {
      * Empty class constructor.
      */
     protected TourGuideV2Application() {
+    }
+    /**
+     * Customize Swagger2 documentation, skipping ErrorController and adding
+     * model classes.
+     *
+     * @return a Docket object
+     */
+    @Bean
+    public Docket swaggerConfiguration() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .paths(PathSelectors.regex("(?!/error.*).*"))
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.tripmaster.TourGuideV2"))
+                .build().apiInfo(infoDetails());
+    }
+
+    private ApiInfo infoDetails() {
+        return new ApiInfo("Tripmaster - Tour Guide 2020 Suite",
+                "What's a wonderful world!", "v2.1", "Free to use",
+                new springfox.documentation.service.Contact("Thierry Schreiner",
+                        "http://doc.tripmaster.com/tourguide",
+                        "DockyDocs@tripmaster.com"),
+                "API License", "http://tripmaster.com",
+                Collections.emptyList());
     }
 
 }
